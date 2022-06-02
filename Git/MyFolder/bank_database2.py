bf80f344c92e11bda6941399255ef353eb2f43b3
Sonia Birate <soniabirate2004@gmail.com>
2022-06-02 17:27:39 -0500

    Added user input as well as fetched the max and most current PersonID and AccountID from database, then incremented those returns.

--- Git/MyFolder/bank_database2.py
+++ Git/MyFolder/bank_database2.py
@@ -10,46 +10,58 @@ if  __name__ == '__main__':
          cnx = mysql.connector.connect(user='bank_app', password='04272004', host='Sonias-MacBook-Air.local', database='bank_database')
          print ("CONNECTED SUCCESSFULLY!")
          #Creating a cursor object using the cursor() method
-         cursor = cnx.cursor()
+         cursor = cnx.cursor(buffered=True)
       except Error as e:
          print("Error while connecting to MySQL", e)
 
+class Customer():
+   def __init__(self, PersonID=None):
+       print ("WELCOME TO OUR BANKING DATABASE")
+       self.LastName = input("Enter the customer's last name: ")
+       self.FirstName = input("Enter the customer's first name: ")
+       self.City = input("Enter the customer's city: ")
+       self.State = input("Enter the customer's state: ")
+       self.DOB = input("Enter the customer's DOB: ")
+       self.MaritalStatus = input("Enter the customer's marital status: ")
+       
+       customer_id = '''SELECT max(PersonID) FROM customer_personal_info;'''
+       cursor.execute(customer_id)
+       result_1 = cursor.fetchone()
+       print(result_1[0])
+       print()
+       if PersonID is None:
+          PersonID = result_1[0]+111
+       self.PersonID = PersonID
 
-class customer():
-   def __init__(self, LastName, FirstName, City, State, MaritalStatus, DOB, PersonID=None):
-      self.LastName = LastName
-      self.FirstName = FirstName
-      self.City = City
-      self.State = State
-      self.DOB = DOB
-      self.MaritalStatus = MaritalStatus
-      if PersonID is None:
-          PersonID = random.randint(1111,9999)
-      self.PersonID = PersonID
-
-class account():
-    def __init__(self, AccountType, AccountBalance, AccountID=None):
+class Account():
+    def __init__(self, AccountID=None, AccountBalance=None):
+        account_id = '''SELECT max(AccountID) FROM customer_account;'''
+        cursor.execute(account_id)
+        result_2 = cursor.fetchone()
+        print(result_2[0])
+        print()
         if AccountID is None:
-          AccountID = random.randint(1111,9999)
+          AccountID = result_2[0]+211   
         self.AccountID = AccountID
-        self.AccountType = AccountType
+        
+        self.AccountType = input("Enter the customer's account type: [Enter Savings or Checking]  ")
+        if AccountBalance is None:
+          AccountBalance = random.randint(100.00,8000.00)
         self.AccountBalance = AccountBalance
 
-class bankDataAcess():
+class BankDataAcess():
     def __init__(self, customer_info, account_info):
-
         self.PersonID = customer_info.PersonID
-
         self.LastName = customer_info.LastName
         self.FirstName = customer_info.FirstName
         self.City = customer_info.City
         self.State = customer_info.State
         self.DOB = customer_info.DOB
         self.MaritalStatus = customer_info.MaritalStatus
-        
         self.AccountID = account_info.AccountID
         self. AccountType = account_info.AccountType
         self.AccountBalance = account_info.AccountBalance
+        print ("Your account balance is " + str(account_info.AccountBalance))
 		
     def accessBank(self): 
         try: 
@@ -72,6 +84,7 @@ class bankDataAcess():
                     
                     # Commit your changes in the database
                 cnx.commit()
+                print(cursor.rowcount, " was inserted.")
 
             except Exception as ex:
                 # Rolling back in case of error
@@ -84,11 +97,16 @@ class bankDataAcess():
             print ("ERROR")
 
 
-connection1 = customer ("Jones", "Tom", "Austin", "Texas", "Single", "2007-05-01")
-print (connection1)
-connection2 = account ("Savings" , 20000.00)
-connection3 = bankDataAcess (connection1,connection2)
-connection3.accessBank()
+
+
+while True:
+    inp = input("Type 'Start' to Start and 'Done' to Stop ")
+    if inp == "Done":
+        break
+    customer = Customer ()
+    account = Account ()
+    connection = BankDataAcess (customer,account)
+    connection.accessBank()
 
 
 
